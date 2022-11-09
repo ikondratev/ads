3eabb511c8dd84bb105c3aaf84526f2bf15bf447
kilya <ilya.kondratyev@kupibilet.ru>
2022-11-09 18:33:16 +0200

    WIP:[0007] Add test for new service

--- services/posting/commands/update_coordinates.rb
+++ services/posting/commands/update_coordinates.rb
@@ -15,15 +15,13 @@ module Posting
         params = yield validation.call(payload)
         yield update_post(params)
 
-        Success
+        Success(true)
       end
 
       private
 
       def update_post(params)
         Try[StandardError] do
-          # post = ads_repo.find_by_id(params[:post_id])
-          # post.merge!(lat: params[:lat], lon: params[:lon])
           ads_repo.update_by_id(params[:post_id], { lon: params[:lon], lat: params[:lat] })
         end.to_result.or(
           Failure([:update_params_error])
